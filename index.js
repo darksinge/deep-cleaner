eabd9959a858e9a3eb12df34790c86cc5b0ab212
Craig Blackburn <cr.blackburn89@gmail.com>
2017-03-15 09:57:19 -0600

    mocha setup

--- index.js
+++ index.js
@@ -6,70 +6,12 @@
 
 var _ = require('lodash');
 
-var Cleaner = function() {};
-
-Cleaner.prototype = Object.create(Object.prototype, {
-  constructor: Cleaner,
-  _removeKey: null,
-  _removeValue: null,
-
-  removeKey: function(obj) {
-    _.forEach(obj, function (key) {
-      (key === this._removeKey && delete obj[key]) || (obj[key] && typeof obj[key] === 'object') && this.clean(obj[key]);
-    });
-    return obj;
-  },
-
-  removeValue: function(obj) {
-    _.forEach(val, function(value, key) {
-      (obj[this._removeValue] == value && delete obj[this._removeValue] || obj[key] === 'object') && this.removeValue(obj[key])
-    });
-    return obj;
-  },
-
-  removeEmpty = function(obj) {
-    _.forEach(obj, function(value, key) {
-      if (typeof value === 'undefined') value = null;
-      if (value === null) {
-        delete obj[key];
-      }
-      if (Array.isArray(value) && value.length === 0) {
-        delete obj[key];
-      }
-      if (JSON.stringify(value) == "{}") {
-        delete obj[key]
-      }
-      if (value === "") {
-        delete obj[key];
-      }
-      this.removeEmpty(obj);
+module.exports = function(obj, removeKey) {
+  function clean(obj, removeKey) {
+    Object.keys(obj).forEach(function(key) {
+      (key === removeKey && obj[key].length === 0) && delete obj[key] ||
+      (obj[key] && typeof obj[key] === 'object') && clean(obj[key], removeKey);
     });
     return obj;
   }
-
-});
-
-module.exports = {
-  clean: function(obj, opts) {
-    var cleaner = new Cleaner();
-    
-    opts instanceof String ? cleaner._removeKey = opts : cleaner._removeKey = opts.removeKey || null;    
-    
-    if (cleaner._removeKey) {
-      cleaner.removeKey(obj);
-    }
-    
-    if (opts.removeValue) {
-      cleaner._removeValue = opts.removeValue;
-      cleaner.removeValue(obj);
-    }
-    
-    if (opts.cleanArrays) cleaner.removeEmpty([])
-    
-    if (opts.cleanObjects) cleaner.cleanObjects = opts.cleanObjects;
-    if (opts.cleanStrings) cleaner.cleanStrings = opts.cleanStrings;
-    if (opts.cloneObject) cleaner.cloneObject = opts.cloneObject;
-    
-    return obj;
-  }
-};
+}
---
+++ test/test.js
@@ -0,0 +1,43 @@
+var cleaner = require('../index');
+var assert = require('assert');
+
+
+
+describe('index.js', function() {
+    describe('#clean()', function() {
+        it('should delete the nested key-value pair where key = "KILL_ALL_THE_CATS"', function(done) {
+            var obj = {
+                "I'm empty!": "",
+                "I am not empty": "stuff",
+                foo: {
+                    bar: "cats",
+                    foo: undefined,
+                    asdf: null,
+                    moreCats: {
+                        'kill all the cats!': "Really, you should do what he says"
+                    },
+                    nestedEmptyObject: {}
+                },
+                "bar": {
+                    description: "I'm a nested 'bar'!",
+                    message: "",
+                    realMessage: "Please don't delete me..."
+                }
+            }
+            
+            // cleaner(obj, 'foo');
+            var removeKey = 'kill all the cats!';
+            function clean(obj) {
+                Object.keys(obj).forEach(function (key) {
+                    console.log('', obj);
+                    (key === 'kill all the cats!' && delete obj[key]) || obj[key] && typeof obj[key] === 'object' && clean(obj[key])
+                });
+                return obj;
+            }
+            
+            assert.equal(undefined, obj['foo']['moreCats']['kill all the cats!']);
+            
+            return done();
+        });
+    });
+});
